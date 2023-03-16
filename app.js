ee7761bcba232bf1b62f25e6dc76589190827a58
salamanderctesiphon <swbrookshire@gmail.com>
2023-03-16 13:32:35 -0500

    copied the factory function example from the previous lesson

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+const personFactory = (name, age) => {
+  const sayHello = () => console.log('hello!');
+  retun { name, age, sayHello};
+};
 
+const jeff = personFactory('jeff', 27);
+
+console.log(jeff.name);
+
+jeff.sayHello();
 
 
